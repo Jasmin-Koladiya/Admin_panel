0afafee9f950d02a5a9c06f3dda21a5a9eeb16e2
jasmin <jasminkoladiya098@gmail.com>
2023-04-07 00:27:51 +0530

    chang

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const port = 8081;
 const app = express();
-const db = require('./config/mongoose');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://jasminkoladiya:jasminkoladiya098@cluster0.bamfqbm.mongodb.net/?retryWrites=true&w=majority" , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("DB connected!");
+}).catch(err => {
+    console.log("Error connecting to DB:", err);
+});
+
 const session = require('express-session')
 const passport = require('passport')
 const passportLocal = require('./config/passport_local_strategy');
 const flash = require('connect-flash');
 var custom = require('./config/middleware');
 const { Cookie } = require('express-session');
+const { default: mongoose } = require('mongoose');
 app.use(express.static('assets'));
 app.use(cookieParser());
 app.use(urlencoded());  
